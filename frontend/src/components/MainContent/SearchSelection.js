3b09c878e0846a0a2a7c88423847a657c2af9908
Cristina Pat <calota.cristina@gmail.com>
2024-06-11 16:47:21 +0100

    feat:added SearchSelection component

---
+++ frontend/src/components/MainContent/SearchSelection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchSection() {
+  return (
+    <div className="search-section">
+      <input type="text" placeholder="Insurance type" />
+      <button className="search-button">Search</button>
+    </div>
+  );
+}
+
+export default SearchSection;
