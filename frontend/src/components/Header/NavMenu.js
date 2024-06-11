9aa00aca4d79a6b04b854f1611749096703e94aa
Cristina Pat <calota.cristina@gmail.com>
2024-06-11 05:57:33 +0100

    feat: added NavMenu component

---
+++ frontend/src/components/Header/NavMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavItem from './NavItem';
+
+function NavMenu() {
+  return (
+    <nav>
+      <NavItem text="Home" />
+      <NavItem text="Fav Pet Insurance Providers" />
+    </nav>
+  );
+}
+
+export default NavMenu;
